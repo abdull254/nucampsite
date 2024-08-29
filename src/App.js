d9abaacf4cbc91c960953a6e805f85dfe75465be
abdullaahiyow <abdullaahiyow@gmail.com>
2024-08-28 21:20:57 -0500

    campsiteSlice.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header'
+import CampsitesList from './features/campsites/CampsitesList';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-              hello nucamp!
+        <Header/>    
+        <CampsitesList/>
+        <Footer/> 
     </div>
   );
 }
 
 export default App;
+
+ 
---
+++ src/components/Footer.js
@@ -0,0 +1,60 @@
+import {Row, Col, Container} from 'reactstrap';
+
+const Footer = () => {
+    return (
+        <footer className='site-footer'>
+                <Container>
+                    <Row>
+                        <Col xs={{ size:4, offset:1}} sm='2'>
+                            <h5>
+                                this will be site navigation!
+                            </h5>
+                        </Col>
+                        <Col xs='6' sm='3' className='text-center'>
+                            <h5>
+                                Social
+                            </h5>
+                            <a
+                                className='btn btn-social-icon btn-instagram'
+                                href='http://instagram.com'>
+                                    <i className="fa fa-instagram"/>
+                                </a>
+                                {' '}
+                                <a 
+                                className="btn btn-social-icon btn-facebook"
+                                href='http://facebook.com'>
+                                    <i  className='fa fa-facebook'/>
+                                </a>{' '}
+                                <a 
+                                    className='btn btn-social-icon btn-twitter'
+                                    href='http://twitter.com'>
+                                        <i className='fa fa-twitter'/>
+                                    </a>
+                                 <a 
+                                    className='btn btn-social-icon btn-googlr'
+                                    href='http://youtube.com'>
+                                        <i className='fa fa-youtube'/>
+                                 </a>
+                        </Col>
+                        <Col className='text-center' sm='4'>
+                            <a
+                             role='button' 
+                             className= "btn btn-link"
+                             href='tel: + 12065551234'
+                             >
+                                <i className='fa fa-phone'/> 1-206-555-1234
+                             </a><br/>
+                             <a
+                             role='button' 
+                             className= "btn btn-link"
+                             href='mailto:notreal@notreal.co'
+                             >
+                                <i className='fa fa-envolope-co'/> campsites@nucamp.co
+                             </a><br/>
+                        </Col>
+                    </Row>
+                </Container>
+        </footer>
+    )
+}
+export default Footer;
